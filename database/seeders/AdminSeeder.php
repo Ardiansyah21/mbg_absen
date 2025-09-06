dec5d6ebe0db30ca3c96308c903574d10c2310a9
Ardiansyah21 <nazrilardiansyah@smkwikrama.sch.id>
2025-09-06 23:48:31 +0700

    Update AdminSeeder email & password

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,17 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash; // <- ini yang kurang
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        if(!User::where('email', 'AdminFatimah@sppg.com')->exists()) {
+        // cek apakah user sudah ada
+        if(!User::where('email', 'admin@domain.com')->exists()) {
             User::create([
                 'name' => 'Admin SPPG',
-                'email' => 'AdminFatimah@sppg.com',
-                'password' => Hash::make('Fatimah123!'), // password aman
+                'email' => 'admin@domain.com',       // email baru
+                'password' => Hash::make('Admin123!'), // password baru, aman karena di-hash
                 'role' => 'admin',
             ]);
         }
