d926d5b88826ee39b83e82cad8aa155a71ae5739
Ardiansyah21 <nazrilardiansyah@smkwikrama.sch.id>
2025-09-18 01:14:45 +0700

    update

--- resources/views/absensi/absen.blade.php
+++ resources/views/absensi/absen.blade.php
@@ -420,7 +420,7 @@
 
                 const kantorLat = -6.691640391234676;
                 const kantorLng = 106.88689131829916;
-                const radiusM = 200;
+                const radiusM = 30;
 
                 function getDistance(lat1, lon1, lat2, lon2) {
                     const R = 6371000;
